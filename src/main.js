5bd09da1828ce7d00517b2b19aca40da0aa01299
Ivan Stickmanov <stickman9000@mail.ru>
2020-12-13 17:20:47 +0300

    First commit

---
+++ src/main.js
@@ -0,0 +1,208 @@
+let url = 'data/paronyms.json';
+// let ls; // localStorage (статистика)
+
+let data; // Все задания
+let param;
+let currentTask; // Текущее задание
+let variant; // Правильный вариант ответа
+let answered = false; // false - ответ ещё не выбран; true - ответ выбран
+
+// Элементы
+const TASK_TEXT = document.querySelector('.context');
+const TASK_HINT = document.querySelector('.hint');
+const TASK_SOURCE = document.querySelector('.source');
+const TASK_BUTTONSLIST = document.querySelector('.buttonslist');
+const TASK_SHOWANSWER = document.querySelector('.showanswer');
+const TASK_EXPLAIN = document.querySelector('.explain');
+const TASK_EXPLAINP = document.querySelector('.explain p');
+
+// Цвета
+const COLOR_GRAY = '#3a3a3a';
+const COLOR_CORRECT = '#70AD47';
+const COLOR_WRONG = '#ED7D31';
+const COLOR_WRONG2 = '#FF3333';
+
+// Получает все задания
+function getData(link) {
+    fetch(link)
+    .then(response => response.json())
+    .then(commits => {
+        data = commits[0].list;
+        param = commits[0].param;
+        //console.log(param);
+        //console.log(data);
+        document.title = param.title + ' — Русский язык';
+        newTask();
+});
+}
+getData(url);
+
+// Подчищает за прошлыми заданиями
+function clearTask() {
+    answered = false;
+    TASK_EXPLAIN.style.display = 'none';
+    TASK_BUTTONSLIST.innerHTML = '';
+    TASK_TEXT.innerHTML = '';
+    TASK_SHOWANSWER.innerText = 'Не знаю';
+    TASK_EXPLAIN.style.display = 'none';
+    TASK_SOURCE.innerHTML = 'Источник: ';
+}
+
+// Генерирует новое задание
+function newTask() {
+    // Вызывает функцию, которая подчищает за прошлыми заданиями
+    clearTask();
+
+    // Выбирает случайное задание
+    currentTask = data[Math.floor(Math.random() * data.length)];
+    //console.log(currentTask);
+
+    // Правильный ответ в отдельную переменную
+    variant = currentTask.variants[0];
+    //console.log(variant);
+
+    // Перемешивает варианты
+    if (param.randomize) {
+        for (let i = currentTask.variants.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [currentTask.variants[i], currentTask.variants[j]] = [currentTask.variants[j], currentTask.variants[i]];
+        }
+    }
+    //console.log(currentTask.variants);
+
+    // Заполняет элемент с текстом и советом
+    TASK_TEXT.innerText = currentTask.text.replace('_', '________');
+    TASK_HINT.innerText = 'Выберите правильный вариант';
+    TASK_HINT.style.color = '#7F7F7F';
+    
+    // Источник
+    if (currentTask.source) {
+        switch (currentTask.source) {
+            case "egefipi":
+                TASK_SOURCE.innerHTML += '<a href="https://fipi.ru/ege/otkrytyy-bank-zadaniy-ege">ege.fipi.ru</a>';
+                break
+            case "osfipi":
+                TASK_SOURCE.innerHTML += '<a href="http://os.fipi.ru/tasks/1/a">os.fipi.ru</a>';
+                break
+            default:
+                TASK_SOURCE.innerHTML += currentTask.source;
+                break
+        }
+    }
+    else {
+        TASK_SOURCE.innerHTML += 'не указан';
+    }
+
+    if (currentTask.egefipi) {
+        TASK_SOURCE.innerHTML = 'Источник: <a href="https://fipi.ru/ege/otkrytyy-bank-zadaniy-ege">из заданий ege.fipi.ru</a>';
+    }
+    if (currentTask.osfipi) {
+        TASK_SOURCE.innerHTML = 'Источник: <a href="http://os.fipi.ru/tasks/1/a">из заданий os.fipi.ru</a>';
+    }
+
+    // Создаёт кнопки
+    for (let i of currentTask.variants) {
+        TASK_BUTTONSLIST.innerHTML += '<button class="answerbuttons" data-index="' + i + '">' + i + '</button>';
+    }
+
+    // добавление addEventListener -> click всем кнопкам
+    for (let i of document.querySelectorAll('.answerbuttons')) {
+        //console.log(i);
+        i.addEventListener('click', function() {
+            checkAnswer(i.dataset.index);
+            //console.log(i.value);
+        });
+    }
+}
+
+// Проверка правильности решения
+function checkAnswer(answer) {
+    TASK_TEXT.innerHTML = TASK_TEXT.innerHTML.replace('________', '<span style="color: ' + COLOR_GRAY + '">' + variant + '</span>');
+
+    if (!answered) {
+        answered = true;
+        TASK_SHOWANSWER.innerText = 'Продолжить';
+        TASK_SHOWANSWER.focus();
+
+        if (answer == variant) {
+            //console.log('Правильно!');
+            TASK_HINT.innerText = 'Правильно!';
+            TASK_HINT.style.color = COLOR_CORRECT;
+            //ls[0] += 1;
+            //updateStat();
+        }
+        else {
+            //console.log('Неправильно!');
+            TASK_HINT.innerText = 'Неправильно!';
+            TASK_HINT.style.color = COLOR_WRONG;
+            //ls[1] += 1;
+            //updateStat();
+
+            // Выделяет неправильный ответ
+            for (let i of document.querySelectorAll('.answerbuttons')) {
+                //console.log(i.dataset.index);
+                if (i.dataset.index == answer) {
+                    i.style.backgroundColor = COLOR_WRONG2;
+                }
+            }
+        };
+
+        // Выделяет правильный ответ
+        for (let i of document.querySelectorAll('.answerbuttons')) {
+            //console.log(i.dataset.index);
+            if (i.dataset.index == variant) {
+                i.style.backgroundColor = COLOR_CORRECT;
+            }
+        }
+
+        // Показывает div с объяснением
+        showExplain();
+    }
+}
+
+// Кнопка "не знаю"
+function idk() {
+    if(!answered) {
+        answered = true;
+        //console.log(variant);
+        TASK_TEXT.innerHTML = TASK_TEXT.innerHTML.replace('________', '<span style="color: ' + COLOR_GRAY + '">' + variant + '</span>');
+        showExplain();
+    }
+    else {
+        showExplain();
+        newTask();
+    }
+}
+
+// Определения слов
+function showExplain() {
+    if (param.explain) {
+        TASK_EXPLAIN.style.display = 'block';
+        TASK_EXPLAINP.innerHTML = '...';
+    }
+}
+
+// Выбор вариантов ответов с помощью клавиш на клавиатуре
+document.addEventListener('keydown', document.addEventListener('keyup', function(event) {
+    if (!isNaN(Number(event.key)) && Number(event.key) != 0 && Number(event.key) <= document.querySelectorAll('.answerbuttons').length) {
+        document.querySelectorAll('.answerbuttons')[event.key - 1].focus();
+    }
+    if (event.code == 'Space' && event.code == 'Enter') {
+        TASK_SHOWANSWER.focus();
+    }
+}));
+
+// HELP
+let helpopen = false;
+let helpbox = document.querySelector('#help');
+helpbox.style.display = 'none';
+document.querySelector('#helplink').addEventListener('click', () => {
+    if (helpopen) {
+        helpbox.style.display = 'none';
+    }
+    else {
+        helpbox.style.display = '';
+    }
+    helpopen = !helpopen;
+    console.log(helpopen);
+});
